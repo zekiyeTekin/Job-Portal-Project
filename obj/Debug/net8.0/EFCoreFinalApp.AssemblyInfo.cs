1a80e4e56c99f082302c658f8d09e00ae6530aaa
zekiyeTekin <97231662+zekiyeTekin@users.noreply.github.com>
2024-10-15 01:20:54 +0300

    fixed gitignore file

--- obj/Debug/net8.0/EFCoreFinalApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EFCoreFinalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreFinalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e37f38ef0899c1a494294bfba97f38199318adb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreFinalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreFinalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
