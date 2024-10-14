41c631a152ca854e116204b0cc9c9a6268eff27a
zekiyeTekin <97231662+zekiyeTekin@users.noreply.github.com>
2024-10-15 01:16:49 +0300

    Removed obj folder from Git tracking

--- obj/Debug/net8.0/EFCoreFinalApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/EFCoreFinalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreFinalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0755cedb75b18f703d200eff1d0a2cfbf613a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e085acb91886218686188706223f48b46a40f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreFinalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreFinalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
