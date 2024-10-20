e9eb57269baa64c95d0660d9922aa6ad12065eb3
zekiyeTekin <97231662+zekiyeTekin@users.noreply.github.com>
2024-10-20 20:26:17 +0300

    The app screen for the job advertisement has been edited

--- Controllers/JobApplyController.cs
+++ Controllers/JobApplyController.cs
@@ -38,6 +38,7 @@ namespace EFCoreFinalApp.Controllers{
 
              var jobApply = new JobApply 
             { 
+                JobPosting = jobPosting,
                 JobPostingId = jobPostingId 
             };
 
